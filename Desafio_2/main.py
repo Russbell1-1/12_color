d1bec6c0400440d9075585b31a7f4ea0ab62d960
CarlosSandoval2025 <carlos.j98232s.s@gmail.com>
2025-06-16 16:55:56 -0500

    Agregamos Ejercicio 2 (Avance)

---
+++ Desafio_2/main.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+
+# This program requires LEGO EV3 MicroPython v2.0 or higher.
+# Click "Open user guide" on the EV3 extension tab for more information.
+
+
+# Create your objects here.
+ev3 = EV3Brick()
+Mi = Motor(Port.A)
+Md = Motor(Port.C)
+color_sensor = ColorSensor(Port.S3)
+robot = DriveBase(Mi,Md, wheel_diameter=55.5, axle_track=104)
+
+Mi.reset.angle(0)
+Md.reset.angle(0)
+
+Mi.run(200)
+Md.run(200)
+
+while color_sensor.color() != Color.RED:
+    Mi.stop()
+    Md.stop()
+
+
+# Write your program here.
